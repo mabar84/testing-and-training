98692761f642070eadfc22bf1231e313cc807e62
HUAWEI <mabar@tut.by>
2023-02-21 20:39:00 +0300

    kabzda20 useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {LearnUseState} from "./learn/LearnUseState";
 
 function App() {
-  return (
+    console.log('App')
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - 23 with VS Code!
-        </a>
+        <LearnUseState/>
+
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React!*/}
+        {/*</a>*/}
       </header>
+
     </div>
   );
 }
