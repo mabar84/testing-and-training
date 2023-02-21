bbd3c8c9a2e92aee637d048386ec1c8a3119e8cd
HUAWEI <mabar@tut.by>
2023-02-21 12:40:07 +0300

    learn react 23

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React - 23!
         </a>
       </header>
     </div>
