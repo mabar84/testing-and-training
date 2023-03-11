ac061040b907e942fe40bc54b1377961caee78f1
HUAWEI <mabar@tut.by>
2023-03-12 01:43:56 +0300

    counter localStorage redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Rating} from "./learn/Rating";
-import {Accordion} from "./learn/Accordion";
-import {ResetUseEffect} from "./learn/ResetUseEffect";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "./bll/store";
+import {incValueAC} from "./bll/counter-reducer";
 
 function App() {
-    console.log('App')
+    console.log('APP')
+    const value = useSelector<AppStateType, number>(state => state.counter.value)
+    const dispatch = useDispatch()
+
+    const incHandler = () => {
+        dispatch(incValueAC())
+    }
+    //
+    //
+    // useEffect(() => {
+    //     let valueAsString = localStorage.getItem('counterValue')
+    //     if (valueAsString) {
+    //         let newValue = JSON.parse(valueAsString)
+    //         setValue(newValue)
+    //     }
+    // }, [])
+    //
+    // useEffect(() => {
+    //     localStorage.setItem('counterValue', JSON.stringify(value))
+    // }, [value])
 
     return (
         <div className="App">
-            <header className="App-header">
-                <ResetUseEffect/>
-                {/*<Rating value={4} title={'RatingTitle'}/>*/}
-                {/*<Accordion title={'First'} collapsed={true}/>*/}
-                {/*<Accordion title={'Second'} collapsed={false}/>*/}
-            </header>
+
+            <h2>{value}</h2>
+            <button onClick={incHandler}>inc</button>
+            {/*<header className="App-header">*/}
+
+
+            {/*<ResetUseEffect/>*/}
+            {/*<Rating value={4} title={'RatingTitle'}/>*/}
+            {/*<Accordion title={'First'} collapsed={true}/>*/}
+            {/*<Accordion title={'Second'} collapsed={false}/>*/}
+            {/*</header>*/}
         </div>
     );
 }
