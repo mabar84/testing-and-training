0bbc0fde003e599f01e488ebd42a9b6c0f82e5b9
HUAWEI <mabar@tut.by>
2023-04-08 00:44:29 +0300

    kabzda 13 - controlled input, checkbox, select

--- src/stories/Inputs.stories.tsx
+++ src/stories/Inputs.stories.tsx
@@ -1,8 +1,11 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Inputs'
 };
+
+export const UncontrolledInput = () => <input/>
 
 export const TrackValueUncontrolledInput = () => {
     const [value, setValue] = useState('')
@@ -19,4 +22,37 @@ export const TrackValueUncontrolledInput = () => {
     </>
 }
 
-export const UncontrolledInput = () => <input/>
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    return <>
+        <input onChange={onChangeInputHandler} value={value}/>
+    </>
+}
+
+export const ControlledCheckbox = () => {
+    const [value, setValue] = useState(false)
+    const onChangeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.checked)
+    }
+    return <>
+        <input type={"checkbox"} onChange={onChangeCheckboxHandler} checked={value}/>
+    </>
+}
+export const ControlledSelect = () => {
+    const [value, setValue] = useState<string>('0')
+    const onChangeSelectHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    return <>
+        <select onChange={onChangeSelectHandler} value={value}>
+            <option value="0">000</option>
+            <option value="1">111</option>
+            <option value="2">222</option>
+            <option value="3">333</option>
+            <option value="4">444</option>
+        </select>
+    </>
+}
