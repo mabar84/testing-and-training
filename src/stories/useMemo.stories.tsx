185e08193bdbdca632871c2dd52f9cf3745188bc
HUAWEI <mabar@tut.by>
2023-05-04 13:24:11 +0300

    kabzda 18 - useMemo is done

---
+++ src/stories/useMemo.stories.tsx
@@ -0,0 +1,38 @@
+import React, {ChangeEvent, useRef, useState, memo, useMemo} from 'react';
+
+export default {
+    title: 'useMemo'
+};
+
+export const useMemoFactorial = () => {
+    console.log('exampleWithFactorial rendered')
+
+    const [a, setA] = useState<number>(1)
+    const [b, setB] = useState<number>(1)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResultA = 1
+        for (let i = 2; i <= a; i++) {
+            let fake = 0
+            while (fake < 50000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA
+    }, [a])
+
+    for (let i = 2; i <= b; i++) {
+        resultB = resultB * i
+    }
+
+    return <>
+        <input type="number" value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/> {a}!={resultA}
+        <br/>
+        <input type="number" value={b} onChange={(e) => setB(+(e.currentTarget.value))}/> {b}!={resultB}
+    </>
+}
