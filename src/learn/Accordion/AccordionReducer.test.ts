8fdd588a87a25cf4ffe7aef38ff91361ef92ceb8
HUAWEI <mabar@tut.by>
2023-05-04 10:46:38 +0300

    kabzda 17 - React.memo is done

--- src/learn/Accordion/AccordionReducer.test.ts
+++ src/learn/Accordion/AccordionReducer.test.ts
@@ -1,10 +1,18 @@
 import {AccordionReducer, action} from './AccordionReducer';
 
-test('toggle collapsed', () => {
-    const startState = {
-        collapsed: false
-    }
+test('switch collapsed on true', () => {
+    const startState = {collapsed: false}
     const endState = AccordionReducer(startState, action)
-
-    expect(endState.collapsed).toBe(!startState.collapsed)
+    expect(endState.collapsed).toBe(true)
+})
+test('switch collapsed on false', () => {
+    const startState = {collapsed: true}
+    const endState = AccordionReducer(startState, action)
+    expect(endState.collapsed).toBe(false)
+})
+test('should throw error', () => {
+    const startState = {collapsed: true}
+    expect(() => {
+        AccordionReducer(startState, {type: 'FAKE'})
+    }).toThrowError()
 })
