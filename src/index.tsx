9f9c71f372d86db4d11f25582ccad5acdfe1f3dd
mabar84 <mabar@tut.by>
2023-09-20 15:31:12 +0300

    del npm, use yarn

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./bll/store";
-import * as ReactDOM from 'react-dom/client';
 import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
