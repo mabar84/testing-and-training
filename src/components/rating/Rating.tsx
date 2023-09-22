29be673f5720a9289fbbfaabe7a118b5d8054e87
mabar84 <mabar@tut.by>
2023-09-23 00:16:32 +0300

    accessKey tryed

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {Star} from "./Star";
-import {styled} from "styled-components";
+import React from "react";
+import { Star } from "./Star";
+import { styled } from "styled-components";
 
 type RatingPropsType = {
-    rating: 0 | 1 | 2 | 3 | 4 | 5
-}
-
-export const Rating = (props: RatingPropsType) => {
-    console.log('Rating rendering');
-    let n = props.rating;
-    let arrSelected = Array.from({length: n}, (_, a) => a + 1);
-    let arrUnSelected = Array.from({length: 5 - n}, (_, a) => a + 1);
-
-    return (
-        <StyledRating>
-            {arrSelected.map((el, ind) => {
-                return <Star key={ind} selected={true}/>
-            })}
-            {arrUnSelected.map((el, ind) => {
-                return <Star key={ind} selected={false}/>
-            })}
-        </StyledRating>
-    )
+  rating: 0 | 1 | 2 | 3 | 4 | 5;
 };
 
-const StyledRating = styled.div`
-`
+export const Rating = (props: RatingPropsType) => {
+  console.log("Rating rendering");
+  let n = props.rating;
+  let arrSelected = Array.from({ length: n }, (_, a) => a + 1);
+  let arrUnSelected = Array.from({ length: 5 - n }, (_, a) => a + 1);
+
+  return (
+    <StyledRating>
+      {arrSelected.map((el, ind) => {
+        return <Star key={ind} selected={true} />;
+      })}
+      <input accessKey="l" />
+      {arrUnSelected.map((el, ind) => {
+        return <Star key={ind} selected={false} />;
+      })}
+    </StyledRating>
+  );
+};
+
+const StyledRating = styled.div``;
