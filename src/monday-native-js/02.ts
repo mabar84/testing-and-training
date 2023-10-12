0ee1a9c42417cba3c2b93d069cd6c18d6258f3de
mabar84 <mabar@tut.by>
2023-10-12 15:57:15 +0300

    monday nativeJS objects is done

--- src/monday-native-js/02.ts
+++ src/monday-native-js/02.ts
@@ -1,51 +1,28 @@
-type CityType = {
+type StreetType = {
     title: string
-    countryTitle: string
 }
 
 type AddressType = {
-    streetTitle: string
-    city: CityType
+    number?: number
+    street: StreetType
 }
 
-type TechType = {
-    id: number
-    title: string
-}
-
-type StudentType = {
-    id: number
-    name: string
-    age: number
-    isActive: boolean
+type HouseType = {
+    buildAt: number
+    repaired: boolean
     address: AddressType
-    technologies: TechType[]
 }
 
-export const student: StudentType = {
-    id: 1,
-    name: 'Dimych',
-    age: 32,
-    isActive: false,
-    address: {
-        streetTitle: 'Surganova 2',
-        city: {
-            title: 'Minsk',
-            countryTitle: 'Belarus'
-        }
-    },
-    technologies: [
-        {
-            id: 1,
-            title: 'HTML'
-        },
-        {
-            id: 2,
-            title: 'CSS'
-        },
-        {
-            id: 3,
-            title: 'React'
-        }
-    ]
+type GovernmentBuildingsType = {
+    type: 'HOSPITAL' | 'FIRE-STATION'
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+export  type CityType = {
+    title: string
+    houses: HouseType[]
+    governmentBuildings: GovernmentBuildingsType[]
+    citizensNumber: number
 }
