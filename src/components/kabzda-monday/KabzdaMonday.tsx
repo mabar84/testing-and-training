0fb6fea10c0a7002dc43c5fe11958c8e83d288f6
mabar84 <mabar@tut.by>
2023-10-26 23:42:35 +0300

    destructuring assignment from learnjs is done

--- src/components/kabzda-monday/KabzdaMonday.tsx
+++ src/components/kabzda-monday/KabzdaMonday.tsx
@@ -11,24 +11,6 @@ type KabzdaMondayPropsType = {
 
 export const KabzdaMonday: React.FC<KabzdaMondayPropsType> = ({h1, ...restProps}) => {
 
-    let obj = {
-        name: 'Dimych',
-        age: 32,
-        lessons: [{title: '1'}, {title: '2'}, {title: '3'}],
-        address: {
-            street: {
-                title: 'Somova'
-            }
-        }
-    }
-
-    const {name, age, address: {street: {title}}} = obj
-    let title2 = obj.address.street.title
-    let [l1, ...rest] = obj.lessons
-    console.clear()
-    console.log(l1)
-    console.log(rest)
-
 
     return (
         <StyledKabzdaMonday>
