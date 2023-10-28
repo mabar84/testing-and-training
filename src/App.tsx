0a306ca083ba0f6c6ef241e5dc6567d770e92fdb
mabar84 <mabar@tut.by>
2023-10-28 14:01:51 +0300

    resolve rerendering app problem with Fullinput

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,83 @@
-import React from 'react';
-import {KabzdaMonday} from './components/kabzda-monday/KabzdaMonday';
-import {Selfeducation} from './components/selfeducation/Selfeducation';
+import React, {useEffect, useRef, useState} from 'react';
+import './App.css';
+import {json} from 'stream/consumers';
+import {Button} from './components/Button';
+import Input from './components/Input';
+import FullInput from './components/FullInput';
 
-const App = () => {
-    console.log('APP');
+
+type PropsType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
+
+function App() {
+
+    console.log('app')
+
+    // const [newTitle, setNewTitle] = useState('')
+
+    // const inputRef = useRef<HTMLInputElement | null>(null)
+
+    const [todos, setTodos] = useState<PropsType[]>([])
+
+    const fetchFoo = () =>
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+
+    useEffect(() => {
+        fetchFoo()
+    }, [])
+
+    // console.log(todos)
+
+    const showHandler = () => fetchFoo()
+    const hideHandler = () => setTodos([])
+
+
+    const addTodo = (m: string) => {
+        const newTodo: PropsType = {
+            userId: 666,
+            id: todos.length + 1,
+            title: m,
+            completed: false
+        }
+        setTodos([newTodo, ...todos])
+    }
 
     return (
         <div className="App">
-            {/*<Selfeducation/>*/}
-            {/*<Kabzda0/>*/}
-            <KabzdaMonday h1={'Monday 3'} address={'Somova'}/>
+            <Button name={'Show'} onClick={showHandler}/>
+            <Button name={'Hide'} onClick={hideHandler}/>
+
+
+            <div>
+                <FullInput buttonName={'addItem2'} onClick={addTodo}/>
+
+                {/*<Input newTitle={newTitle} setNewTitle={setNewTitle}/>*/}
+                {/*<Button name={'Add'} onClick={addTodo}/>*/}
+            </div>
+
+            <ul>
+                {
+                    todos.map(el => {
+                        return (
+                            <li key={el.id}>
+                                <input type="checkbox" checked={el.completed}/>
+                                <span>{el.title}</span> -
+                                <span>{el.id}</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
         </div>
     );
-};
+}
 
 export default App;
+
