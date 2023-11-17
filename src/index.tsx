583ec83382d1b8db8679b729fedab9b2c099c181
mabar84 <mabar@tut.by>
2023-11-17 23:32:34 +0300

    router.tsx additional class with Igor

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {createRoot} from 'react-dom/client';
+import {BrowserRouter, RouterProvider} from "react-router-dom";
+import {router} from "./routes/router";
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App/>);
-// root.render(<Provider store={store}><App/></Provider>);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <RouterProvider router={router}/>
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>
 
-
-// const root = ReactDOM.createRoot(
-//     document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//     //  <React.StrictMode>
-//     <Provider store={store}><App/></Provider>
-//     //   </React.StrictMode>
-// );
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
