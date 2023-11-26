d49d53697b8158dec95e68365382c368ba9e9173
mabar84 <mabar@tut.by>
2023-11-26 15:25:53 +0300

    custom select added to KabzdaTuesday

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {KabzdaMonday} from './components/kabzda-monday/KabzdaMonday';
+import {KabzdaTuesday} from './components/kabzda-tuesday/KabzdaTuesday';
 
 const App = () => {
     console.log('APP');
@@ -7,7 +8,8 @@ const App = () => {
     return (
         <div className="App">
             {/*<Kabzda0/>*/}
-            <KabzdaMonday h1={'Monday 4'} address={'Somova'}/>
+            {/*<KabzdaMonday h1={'Monday 4'} address={'Somova'}/>*/}
+            <KabzdaTuesday/>
         </div>
     );
 };
