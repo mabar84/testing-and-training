cbcad222247206431ecf669fbb399d399b93cf98
mabar84 <mabar@tut.by>
2023-12-01 01:33:42 +0300

    n1 fixed

--- src/tuesday-native-js/lesson_8.ts
+++ src/tuesday-native-js/lesson_8.ts
@@ -2,10 +2,8 @@
 // числа (неопределённое кол-во) и возвращает их сумму (rest).
 
 export function sum(...nums: Array<number>): number {
-    const a = arguments;
     //@ts-ignore
-    const b = [...a];
-    return b.reduce((a, b) => a + b)
+    return [...arguments].reduce((a, b) => a + b)
 }
 
 // 2. Функция getTriangleType принимает три параметра:
@@ -56,18 +54,31 @@ export const isEvenIndexSumGreater = (arr: Array<number>): boolean => {
 
 export function getSquarePositiveIntegers(array: Array<number>): Array<number> {
     return array.filter(el => (el % 1 === 0) && (el > 0)).map(n => n * n)
+    // return array.reduce((acc: Array<number>, el: number) => {
+    //         if ((el % 1 === 0) && (el > 0)) {
+    //             acc.push(el * el)
+    //         }
+    //         return acc
+    //     }, []
+    // )
 }
 
 // 6. Функция принимает параметром целое не отрицательное число N и возвращает сумму всех чисел от 0 до N включительно
 // Попробуйте реализовать функцию без использования перебирающих методов.
 
 export function sumFirstNumbers(N: number): number {
-    let s = 0;
-    while (N > 0) {
-        s += N
-        N--
-    }
-    return s
+    // let s = 0;
+    // let s = 0;
+    // while (N > 0) {
+    //     s += N
+    //     N--
+    // }
+    // return s
+
+    if (!N) return 0
+    return N + sumFirstNumbers(N - 1);
+
+    // return N * (N + 1) / 2;
 }
 
 // ...и "лапку" вверх!!!!
