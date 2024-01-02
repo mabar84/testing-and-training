a7d9c78f19015c5e32d5d0d3ab976b964af10586
mabar84 <mabar@tut.by>
2024-01-03 00:39:03 +0300

    WithKeyPress added

--- src/components/kabzda-wednesday/useEffectCleanUp/useEffectCleanUp.tsx
+++ src/components/kabzda-wednesday/useEffectCleanUp/useEffectCleanUp.tsx
@@ -1,7 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
 export const UseEffectCleanUp = () => {
-    console.log('UseEffectCleanUp')
+
+    return <>
+        <WithCounter/>
+        <WithKeyPress/>
+    </>
+
+};
+
+const WithCounter = () => {
+    console.log('WithCounter')
     const [counter, setCounter] = useState(1)
 
     useEffect(() => {
@@ -10,10 +19,36 @@ export const UseEffectCleanUp = () => {
             console.log('useEffect cleanUp with counter ', counter)
         }
     }, [counter])
-
     return (
         <div>
             <button onClick={() => setCounter(state => state + 1)}>{counter}</button>
         </div>
     );
-};
+}
+
+
+const WithKeyPress = () => {
+    const [text, setText] = useState('')
+    console.log('WithKeyPress rendered with text ', text)
+
+
+    useEffect(() => {
+        console.log('useEffect starts with text ', text)
+
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+        return () => {
+            console.log('useEffect cleanUp with text ', text)
+            window.removeEventListener('keypress', handler)
+        }
+    }, [text])
+    return (
+        <div>
+            Typed text: {text}
+        </div>
+    );
+}
