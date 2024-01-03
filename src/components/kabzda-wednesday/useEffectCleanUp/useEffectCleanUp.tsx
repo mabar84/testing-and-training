b9fb831470e1dface4bd6f0355f7908576844402
mabar84 <mabar@tut.by>
2024-01-03 17:51:49 +0300

    SetTimeoutExample added, kabzda25 is done

--- src/components/kabzda-wednesday/useEffectCleanUp/useEffectCleanUp.tsx
+++ src/components/kabzda-wednesday/useEffectCleanUp/useEffectCleanUp.tsx
@@ -3,8 +3,9 @@ import React, {useEffect, useState} from 'react';
 export const UseEffectCleanUp = () => {
 
     return <>
-        <WithCounter/>
-        <WithKeyPress/>
+        {/*<WithCounter/>*/}
+        {/*<WithKeyPress/>*/}
+        <SetTimeoutExample/>
     </>
 
 };
@@ -26,7 +27,6 @@ const WithCounter = () => {
     );
 }
 
-
 const WithKeyPress = () => {
     const [text, setText] = useState('')
     console.log('WithKeyPress rendered with text ', text)
@@ -46,6 +46,29 @@ const WithKeyPress = () => {
             window.removeEventListener('keypress', handler)
         }
     }, [text])
+    return (
+        <div>
+            Typed text: {text}
+        </div>
+    );
+}
+
+const SetTimeoutExample = () => {
+    const [text, setText] = useState('')
+    console.log('SetTimeoutExample ', text)
+
+
+    useEffect(() => {
+        console.log('useEffect starts with text ', text)
+        const setTimeoutId = setTimeout(() => {
+            console.log('Timeout expired')
+            setText('3 seconds passed')
+        }, 3000)
+        return () => {
+            console.log('useEffect cleanUp with text ', text)
+            clearTimeout(setTimeoutId)
+        }
+    }, [])
     return (
         <div>
             Typed text: {text}
