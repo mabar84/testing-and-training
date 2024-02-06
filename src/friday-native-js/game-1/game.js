ed57d7b99d4194375506ddcfb239fc61ecc92b68
mabar84 <mabar@tut.by>
2024-02-06 20:45:03 +0300

    friday game-2 added

--- src/friday-native-js/game-1/game.js
+++ src/friday-native-js/game-1/game.js
@@ -1,109 +1,230 @@
 class Game {
-  #settings = {
-    gridSize: {
-      columns: 4,
-      rows: 4,
-    },
-  };
-  #status = "pending";
-  #player1;
-  #player2;
-  #google;
+    #settings = {
+        gridSize: {
+            columns: 4,
+            rows: 4,
+        },
+        googleJumpInterval: 2000,
+    };
+    #status = "pending";
+    #player1;
+    #player2;
+    #google;
+    #score = {
+        1: 0,
+        2: 0,
+    };
 
-  constructor() {}
-
-  getRandomPosition(coordinates) {
-    let x, y;
-
-    // Generate random coordinates until a unique pair is found
-    do {
-      x = NumberUtil.getRandomNumber(this.#settings.gridSize.columns);
-      y = NumberUtil.getRandomNumber(this.#settings.gridSize.rows);
-    } while (
-      coordinates.some(
-        (coord) => coord.position.x === x && coord.position.y === y
-      )
-    );
-
-    return { x, y };
-  }
-
-  set settings(settings) {
-    this.#settings = settings;
-  }
-  get settings() {
-    return this.#settings;
-  }
-
-  get player1() {
-    return this.#player1;
-  }
-  get player2() {
-    return this.#player2;
-  }
-  get google() {
-    return this.#google;
-  }
-
-  get status() {
-    return this.#status;
-  }
-  start() {
-    if (this.#status === "pending") {
-      this.#player1 = new Player({
-        x: NumberUtil.getRandomNumber(this.#settings.gridSize.columns),
-        y: NumberUtil.getRandomNumber(this.#settings.gridSize.rows),
-      });
-      this.#player2 = new Player(this.getRandomPosition([this.#player1]));
-      this.#google = new Google(
-        this.getRandomPosition([this.#player1, this.#player2])
-      );
-      this.#status = "in-progress";
+    constructor() {
     }
-  }
-  pause() {
-    if (this.#status === "in-progress") {
-      this.#status = "paused";
+
+    getRandomPosition(coordinates) {
+        let x, y;
+
+        // Generate random coordinates until a unique pair is found
+        do {
+            x = NumberUtil.getRandomNumber(this.#settings.gridSize.columns);
+            y = NumberUtil.getRandomNumber(this.#settings.gridSize.rows);
+        } while (
+            coordinates.some(
+                (coord) => coord.position.x === x && coord.position.y === y
+            )
+            );
+
+        return {x, y};
     }
-  }
-  resume() {
-    if (this.#status === "paused") {
-      this.#status = "in-progress";
+
+    set settings(settings) {
+        this.#settings = settings;
     }
-  }
+
+    get settings() {
+        return this.#settings;
+    }
+
+    get player1() {
+        return this.#player1;
+    }
+
+    get player2() {
+        return this.#player2;
+    }
+
+    get google() {
+        return this.#google;
+    }
+
+    get status() {
+        return this.#status;
+    }
+
+    #createdUnitsPosition() {
+        this.#player1 = new Player(
+            new Position({
+                x: NumberUtil.getRandomNumber(this.#settings.gridSize.columns),
+                y: NumberUtil.getRandomNumber(this.#settings.gridSize.rows),
+            }),
+            1
+        );
+        this.#player2 = new Player(
+            new Position(this.getRandomPosition([this.#player1])),
+            2
+        );
+        this.#google = new Google(
+            new Position(this.getRandomPosition([this.#player1, this.#player2]))
+        );
+    }
+
+    async start() {
+        if (this.#status === "pending") {
+            this.#createdUnitsPosition();
+            this.#status = "in-progress";
+            setInterval(() => {
+                this.#jumpGoogleToAnotherPosition();
+            }, this.#settings.googleJumpInterval);
+        }
+    }
+
+    #jumpGoogleToAnotherPosition() {
+        this.#google = new Google(
+            new Position(
+                this.getRandomPosition([this.#player1, this.#player2, this.#google])
+            )
+        );
+    }
+
+    // 1,1  1,2
+    // 2,1  2,2
+
+    #checkBorder(player, delta) {
+        const newPosition = player.position.clone();
+        if (delta.x) newPosition.x += delta.x;
+        if (delta.y) newPosition.y += delta.y;
+
+        if (newPosition.x < 1 || newPosition.x > this.#settings.gridSize.columns) {
+            return true;
+        }
+        if (newPosition.y < 1 || newPosition.y > this.#settings.gridSize.rows) {
+            return true;
+        }
+
+        return false;
+    }
+
+    #checkOtherPlayer(movingPlayer, anotherPlayer, delta) {
+        const newPosition = movingPlayer.position.clone();
+        if (delta.x) newPosition.x += delta.x;
+        if (delta.y) newPosition.y += delta.y;
+
+        return newPosition.position.equal(anotherPlayer);
+    }
+
+    #checkGoogleCatching(player) {
+        if (player.position.equal(this.#google.position)) {
+            this.#score[player.id]++;
+            jumpGoogleToAnotherPosition();
+        }
+    }
+
+    // TODO зарефакторить передвижения игроков
+
+    movePlayer1Right() {
+        const delta = {x: 1};
+
+        const isBorder = this.#checkBorder(this.player1, delta);
+        const isAnotherPlayer = this.#checkOtherPlayer(
+            this.player1,
+            this.player2,
+            delta
+        );
+
+        if (isBorder || isAnotherPlayer) {
+            return;
+        }
+
+        this.#player1.position.x++;
+
+        this.#checkGoogleCatching(this.player1);
+    }
+
+    movePlayer1Left() {
+        const delta = {x: -1};
+
+        const isBorder = this.#checkBorder(this.player1, delta);
+        const isAnotherPlayer = this.#checkOtherPlayer(
+            this.player1,
+            this.player2,
+            delta
+        );
+
+        if (isBorder || isAnotherPlayer) {
+            return;
+        }
+
+        this.#player1.position.x++;
+
+        this.#checkGoogleCatching(this.player1);
+    }
+
+    movePlayer2Right() {
+    }
+
+    movePlayer2Left() {
+    }
+
+    pause() {
+        if (this.#status === "in-progress") {
+            this.#status = "paused";
+        }
+    }
+
+    resume() {
+        if (this.#status === "paused") {
+            this.#status = "in-progress";
+        }
+    }
 }
 
 class NumberUtil {
-  static getRandomNumber(max) {
-    return Math.floor(Math.random() * (max - 1 + 1) + 1);
-  }
+    static getRandomNumber(max) {
+        return Math.floor(Math.random() * max + 1);
+    }
 }
 
 class Unit {
-  constructor(position) {
-    this.position = position;
-  }
+    constructor(position) {
+        this.position = position;
+    }
 }
 
 class Player extends Unit {
-  constructor(position) {
-    super(position);
-  }
+    constructor(position, id) {
+        super(position);
+        id: id;
+    }
 }
 
 class Google extends Unit {
-  constructor(position) {
-    super(position);
-  }
+    constructor(position) {
+        super(position);
+    }
 }
 
 class Position {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+    constructor(obj) {
+        this.x = obj.x;
+        this.y = obj.y;
+    }
+
+    clone() {
+        return new Position({x: this.x, y: this.y});
+    }
+
+    equal(positionToCompare) {
+        return this.x === positionToCompare.x && this.y === positionToCompare.y;
+    }
 }
 
 module.exports = {
-  Game,
+    Game,
 };
--- src/friday-native-js/game-1/game.test.js
+++ src/friday-native-js/game-1/game.test.js
@@ -34,36 +34,89 @@ describe("game tests", () => {
     expect(game.status).toBe("in-progress");
   });
 
-  it("player1 and player2 should have unique coordinates", () => {
-    const game = new Game();
+  it("player1, player2 and google should have unique coordinates", () => {
+    for (let i = 0; i < 10; i++) {
+      const game = new Game();
+      game.settings = {
+        gridSize: {
+          columns: 2,
+          rows: 3,
+        },
+      };
 
+      (async () => {
+        await game.start();
+      })();
+
+      console.log("game.player1", game.player1);
+      console.log("game.player2", game.player2);
+      // console.log("game.google", game.google);
+
+      expect([1, 2]).toContain(game.player1.position.x);
+      expect([1, 2, 3]).toContain(game.player1.position.y);
+
+      expect([1, 2]).toContain(game.player2.position.x);
+      expect([1, 2, 3]).toContain(game.player2.position.y);
+
+      expect([1, 2]).toContain(game.google.position.x);
+      expect([1, 2, 3]).toContain(game.google.position.y);
+
+      expect(
+        game.google.position.x !== game.player1.position.x ||
+          (game.google.position.y !== game.player1.position.y &&
+            game.google.position.x !== game.player2.position.x) ||
+          game.google.position.y !== game.player2.position.y
+      );
+    }
+  });
+
+  it("check google position after jump", async () => {
+    const game = new Game();
     game.settings = {
       gridSize: {
-        columns: 2,
-        rows: 3,
+        columns: 1,
+        rows: 4,
       },
+      googleJumpInterval: 100,
     };
 
-    game.start();
+    await game.start();
 
-    console.log("game.player1", game.player1);
-    console.log("game.player2", game.player2);
-    console.log("game.google", game.google);
+    const prevPosition = game.google.position.clone();
+    // console.log("prevPosition", prevPosition);
+    await sleep(150);
+    // console.log("game.google.position", game.google.position);
+    expect(game.google.position.equal(prevPosition)).toBe(false);
+  });
 
-    expect([1, 2]).toContain(game.player1.position.x);
-    expect([1, 2, 3]).toContain(game.player1.position.y);
+  it("catch google by player1 or player2", async () => {
+    const game = new Game();
+    game.settings = {
+      gridSize: {
+        columns: 3,
+        rows: 1,
+      },
+      // googleJumpInterval: 100,
+    };
+    // p1 p2 g || p1 g p2 || p2 p1 g || p2 g p1 || g p1 p2 || g p2 p1
 
-    expect([1, 2]).toContain(game.player2.position.x);
-    expect([1, 2, 3]).toContain(game.player2.position.y);
+    const deltaForPlayer1 = game.google.position.x - game.player1.position.x;
 
-    expect([1, 2]).toContain(game.google.position.x);
-    expect([1, 2, 3]).toContain(game.google.position.y);
+    if (Math.abs(deltaForPlayer1) === 2) {
+      const deltaForPlayer2 = game.google.position.x - game.player2.position.x;
+      if (deltaForPlayer2 > 0) {
+        game.movePlayer2Right();
+      } else {
+        game.movePlayer2Left();
+      }
+    }
 
-    expect(
-      game.google.position.x !== game.player1.position.x ||
-        (game.google.position.y !== game.player1.position.y &&
-          game.google.position.x !== game.player2.position.x) ||
-        game.google.position.y !== game.player2.position.y
-    );
+    expect();
   });
 });
+
+function sleep(ms) {
+  return new Promise((res) => {
+    setTimeout(res, ms);
+  });
+}
